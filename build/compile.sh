67be8d3321147805c58edd69678a810a4ff7708d
Jordan Santell <jsantell@gmail.com>
2011-10-07 02:15:19 -0400

    Change build script for new directory structure

--- build/compile.sh
+++ build/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 java -jar compiler.jar \
---js_output_file=../js/jquery.presentr.min.js \
+--js_output_file=../jquery.presentr.min.js \
 --compilation_level=SIMPLE_OPTIMIZATIONS \
---js=../js/jquery.presentr.js
+--js=../jquery.presentr.js
